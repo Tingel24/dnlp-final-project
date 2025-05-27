bcd49e5b5b88a1ef21a481f6835c044a300ffd9d
Niklas Bauer <nikbau2000@gmail.com>
2025-05-27 09:40:14 +0900

    Update run_gpu_sanity_check.sh

--- run_gpu_sanity_check.sh
+++ run_gpu_sanity_check.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=gpu_sanity_check
 #SBATCH -t 00:00:10                  # estimated time # TODO: adapt to your needs
 #SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
-#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/compute_partitions/gpu_partitions/index.html for more options
+#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/how_to_use/compute_partitions/gpu_partitions/index.html for more options
 #SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
 #SBATCH --nodes=1                    # total number of nodes
 #SBATCH --ntasks=1                   # total number of tasks
